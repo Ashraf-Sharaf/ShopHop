ccf78b8398940ddab0c7e69029aacf04855bc144
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2025-06-30 00:46:53 +0300

    backend- adding missing jwt import to authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,8 @@
+const jwt = require("jsonwebtoken");
+
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
+console.log("1")
   if (!authHeader?.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token provided" });
   }
@@ -8,9 +10,11 @@ const authMiddleware = (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
+    console.log("2")
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
+    console.log("3")
   } catch (error) {
     return res.status(401).json({ message: "Token is invalid or expired" });
   }
