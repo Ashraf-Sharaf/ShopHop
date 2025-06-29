4783eaddcdcb46a337de5985d75e3f7cc10a6d8b
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2025-06-30 01:58:02 +0300

    backend- moving admin only product routes to admin routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { body, param } = require("express-validator");
+
 const authMiddleware = require("../middleware/authMiddleware");
+const validateMiddleware = require("../middleware/validateMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
+
 const adminController = require("../controllers/adminController");
+const productController = require("../controllers/productController");
 
 router.get(
   "/summary",
@@ -11,8 +16,61 @@ router.get(
   adminController.getSummary
 );
 
-router.get("/all_users", authMiddleware, adminMiddleware, adminController.getAllUsers);
+router.get(
+  "/all_users",
+  authMiddleware,
+  adminMiddleware,
+  adminController.getAllUsers
+);
 
-router.get("/all_orders", authMiddleware, adminMiddleware, adminController.getAllOrders);
+router.get(
+  "/all_orders",
+  authMiddleware,
+  adminMiddleware,
+  adminController.getAllOrders
+);
+
+router.post(
+  "/product/add",
+  authMiddleware,
+  adminMiddleware,
+  [
+    body("name").trim().notEmpty().withMessage("Name is required"),
+    body("category").trim().notEmpty().withMessage("Category is required"),
+    body("price").isNumeric().withMessage("Price must be a number"),
+    body("stock")
+      .optional()
+      .isInt({ min: 0 })
+      .withMessage("Stock must be a non-negative integer"),
+    body("image").optional().isString(),
+    body("description").optional().isString(),
+  ],
+  validateMiddleware,
+  productController.addProduct
+);
+
+router.put(
+  "/product/edit/:id",
+  authMiddleware,
+  adminMiddleware,
+  [
+    body("price").optional().isNumeric().withMessage("Price must be a number"),
+    body("stock")
+      .optional()
+      .isInt({ min: 0 })
+      .withMessage("Stock must be a non-negative integer"),
+  ],
+  validateMiddleware,
+  productController.editProduct
+);
+
+router.delete(
+  "product/delete/:id",
+  authMiddleware,
+  adminMiddleware,
+  [param("id").isMongoId().withMessage("Invalid product ID")],
+  validateMiddleware,
+  productController.deleteProduct
+);
 
 module.exports = router;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const { body, param} = require("express-validator");
+const { param } = require("express-validator");
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const adminMiddleware = require("../middleware/adminMiddleware");
 const validateMiddleware = require("../middleware/validateMiddleware");
 const productController = require("../controllers/productController");
 
@@ -13,49 +11,6 @@ router.get(
   [param("id").isMongoId().withMessage("Invalid product ID")],
   validateMiddleware,
   productController.getProduct
-);
-
-router.post(
-  "/",
-  authMiddleware,
-  adminMiddleware,
-  [
-    body("name").trim().notEmpty().withMessage("Name is required"),
-    body("category").trim().notEmpty().withMessage("Category is required"),
-    body("price").isNumeric().withMessage("Price must be a number"),
-    body("stock")
-      .optional()
-      .isInt({ min: 0 })
-      .withMessage("Stock must be a non-negative integer"),
-    body("image").optional().isString(),
-    body("description").optional().isString(),
-  ],
-  validateMiddleware,
-  productController.addProduct
-);
-
-router.put(
-  "/:id",
-  authMiddleware,
-  adminMiddleware,
-  [
-    body("price").optional().isNumeric().withMessage("Price must be a number"),
-    body("stock")
-      .optional()
-      .isInt({ min: 0 })
-      .withMessage("Stock must be a non-negative integer"),
-  ],
-  validateMiddleware,
-  productController.editProduct
-);
-
-router.delete(
-  "/:id",
-  authMiddleware,
-  adminMiddleware,
-  [param("id").isMongoId().withMessage("Invalid product ID")],
-  validateMiddleware,
-  productController.deleteProduct
 );
 
 module.exports = router;
