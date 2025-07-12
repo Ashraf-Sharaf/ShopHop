e35eaf0e0211650ce9c1a66d9e78a39cda777932
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2025-07-12 21:20:09 +0300

    frontend-editing navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,42 @@
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, IconButton, Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LoginIcon from "@mui/icons-material/Login";
 
 export default function Navbar() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
-            ShopHop
-          </Link>
-        </Typography>
-        <Button color="inherit" component={Link} to="/login">Login</Button>
-        <Button color="inherit" component={Link} to="/cart">Cart</Button>
+        <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
+          <Typography variant="h6" sx={{ mr: 4 }}>
+            <Link
+              to="/"
+              style={{
+                color: "white",
+                textDecoration: "none",
+                fontStyle: "italic",
+                fontWeight: "bold",
+                letterSpacing: "0.5px",
+              }}
+            >
+              ShopHop
+            </Link>
+          </Typography>
+
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/products">
+            Products
+          </Button>
+        </Box>
+
+        <IconButton color="inherit" component={Link} to="/login">
+          <LoginIcon />
+        </IconButton>
+        <IconButton color="inherit" component={Link} to="/cart">
+          <ShoppingCartIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
